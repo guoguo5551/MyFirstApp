fa2543e73744a7c1ac5f4666ba120c66d84e65b8
guoguo5551 <guoguo5551@users.noreply.github.com>
2017-11-01 00:34:20 +0800

    update view.

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clickView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        clickView.isUserInteractionEnabled=true
+        let TapGesture=UITapGestureRecognizer(target: self, action: "clickViewF")
+        self.clickView.addGestureRecognizer(TapGesture)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func clickViewF(){
+        print("clicked")
+        let alert = UIAlertController(title:"Message",message:"image",preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title:"OK",style:UIAlertActionStyle.default)
+        alert.addAction(okAction)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
